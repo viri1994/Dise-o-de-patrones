32378d1dd438a68147d39294c2e58b55bfd53892
viri1994 <vdlre1994@gmail.com>
2019-05-14 14:12:27 -0500

    Agregamos views para poderlo mostrar en el navegador

--- controllers/search.js
+++ controllers/search.js
@@ -1,16 +1,39 @@
 'use strict'
 const modelSearch=require('../models/modelSearch.js');
-const search=(req)=>{
+const fs = require('fs');
+var path = require ('path');
+var appDir = path.dirname(require.main.filename);
+var ruta = path.join('/views', 'vista.html')
+
+
+const htmlRender = (html, data) => { console.log (htmlRender);
+    let parsedHtml = html.toString('utf8');
+
+    /*for(key in data){
+        let exp = "{{"+key+"}}";
+        let reg = new RegExp(exp, 'g');
+        parsedHtml = parsedHtml.replace(reg, data[key]);
+    }
+
+	return parsedHtml;*/
+};
+
+	const search=(req)=>{
 	var result=modelSearch(); 
 	var nombre = 'Viridiana';
 	result=result.filter((alumno) => { 
-		if(alumno.nombre===nombre)
+		if(alumno.nombre===nombre){
 			{return result;}
+		}
 
 	
 
 	})
-	return(JSON.stringify(result));
+	const view = fs.readFileSync(appDir+ ruta).toString('utf8');
+	
+
+	//const parsedHtml = htmlRender(view, result[0]);
+	return(view);
 	
 		
 
